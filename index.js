a4e7aef875f041bcecebe651cf8ac5a41c886e7b
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-05-20 19:52:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,40 @@ class Dog {}
 class Bird {}
 
 speak();
+
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak() {
+        return `${this.name} says meow!`
+    }
+}
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak() {
+        return `${this.name} says woof!`
+    }
+}
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak () {
+        if (this.sex == 'male') {
+            return `It's me! ${this.name}, the parrot!`
+        } else {
+            return `${this.name} says squawk!`
+        }
+    }
+}
